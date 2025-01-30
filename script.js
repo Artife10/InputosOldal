14a3d0b2b239e7f206a3dd8bb8b969f7d4e8cc08
Artifex <krisztianka78.4@gmail.com>
2025-01-30 10:13:48 +0100

    Mestermű

--- script.js
+++ script.js
@@ -6,9 +6,35 @@ let email = document.getElementById("email")
 function SubmitForm() {
     if (nem && nev.value != "" && datum.value != "" && email.value != "") {
         document.getElementById("siker").innerHTML = "<h1>SIKERES!!</h1>";
+        document.getElementById("neverror").innerHTML = "";
+        document.getElementById("daterror").innerHTML = "";
+        document.getElementById("emailrror").innerHTML = "";
+        document.getElementById("nemerror").innerHTML = "";
     }
     else{
-        alert("Töltsd ki az űrlapot")
+        if (nev.value == "") {
+            document.getElementById("neverror").innerHTML = "Tölsd ki!";
+        }else{
+            document.getElementById("neverror").innerHTML = "";
+        }
+
+        if (datum.value == "") {
+            document.getElementById("daterror").innerHTML = "Tölsd ki!";
+        }else{
+            document.getElementById("daterror").innerHTML = "";
+        }
+
+        if (email.value == "") {
+            document.getElementById("emailrror").innerHTML = "Tölsd ki!";
+        }else{
+            document.getElementById("emailrror").innerHTML = "";
+        }
+
+        if (nem == false) {
+            document.getElementById("nemerror").innerHTML = "Tölsd ki!";
+        }else{
+            document.getElementById("nemerror").innerHTML = "";
+        }
     }
 }
 
