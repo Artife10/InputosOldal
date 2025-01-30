6bc16531b86085221209edb8fce438b7f084ce13
Artifex <krisztianka78.4@gmail.com>
2025-01-30 10:35:17 +0100

    sdsd
    sdsd

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let nem = false;
 let nev = document.getElementById("nev")
 let datum = document.getElementById("szul")
 let email = document.getElementById("email")
+let leanyos = false
 
 function SubmitForm() {
-    if (nem && nev.value != "" && datum.value != "" && email.value != "") {
+
+    if (nem && leanyos && nev.value != "" && datum.value != "" && email.value != "" && document.getElementById('lnev').value != "") {
         document.getElementById("siker").innerHTML = "<h1>SIKERES!!</h1>";
         document.getElementById("neverror").innerHTML = "";
         document.getElementById("daterror").innerHTML = "";
         document.getElementById("emailrror").innerHTML = "";
         document.getElementById("nemerror").innerHTML = "";
+        document.getElementById('lneverror').innerHTML = ""
     }
     else{
         if (nev.value == "") {
@@ -30,6 +33,12 @@ function SubmitForm() {
             document.getElementById("emailrror").innerHTML = "";
         }
 
+        if (document.getElementById('lnev').value == "") {
+            document.getElementById('lneverror').innerHTML = "Töltsd ki";
+        }else{
+            document.getElementById('lnemerror').innerHTML = "";
+        }
+
         if (nem == false) {
             document.getElementById("nemerror").innerHTML = "Tölsd ki!";
         }else{
@@ -40,13 +49,15 @@ function SubmitForm() {
 
 
 document.getElementById("no").addEventListener("click", function() {
-
-    document.getElementById("neves").innerText = "Leánykori név: "
     nem = true;
+    leanyos = true
+    document.getElementById("leanyos").innerHTML = "<label for='leanyosnev' id='leanyosnev'>Leánykorinév: </label> <input type='text' id='lnev' placeholder='nev'>   <p id='lneverror'></p><br>"
+
     
 })
 
 document.getElementById("ferfi").addEventListener("click", function() {
-    document.getElementById("neves").innerText = "Név: "
      nem = true;
+     leanyos = true
+     document.getElementById("leanyos").innerHTML = ""
 })
